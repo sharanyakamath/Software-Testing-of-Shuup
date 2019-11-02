d4b405bc73263937e95c0fdc3de0119904dd5883
smarty1palak <smarty1palak@gmail.com>
2019-11-02 16:36:56 +0530

    test cases

---
+++ test-cases/19.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+browser = webdriver.Chrome()
+browser.get("http://127.0.0.1:8000/sa/login/?next=/sa/") 
+time.sleep(1)
+username = browser.find_element_by_id("id_username")
+password = browser.find_element_by_id("id_password")
+username.send_keys("palak")
+password.send_keys("palak123")
+login_attempt = browser.find_element_by_xpath("//*[@type='submit']")
+login_attempt.submit()
+time.sleep(1)
+browser.get("http://127.0.0.1:8000/sa/discounts/")
+browser.find_element_by_xpath("/html/body/div[1]/div[2]/div/div/div/form/div/a[1]").click()
+time.sleep(1)
+browser.find_element_by_xpath("//*[@id='id_name']").send_keys('notebook')
+browser.find_element_by_xpath("//*[@id='id_start_datetime']").send_keys('2019-10-18 17:18')
+browser.find_element_by_xpath("//*[@id='id_end_datetime']").send_keys('2019-10-31 17:18')
+browser.find_element_by_xpath("/html/body/div[1]/div[2]/div/div/div/form/div/div/button[1]").click()
+browser.find_element_by_xpath("/html/body/div[1]/div[2]/div/div/div/form/div/div/button[1]").click()
