0925101a492c7380b196c431cae99fb6e9ea4d48
sharanyakamath <sherrykamath@gmail.com>
2019-11-03 00:42:49 +0530

    testcases added

---
+++ test-cases/36.py
@@ -0,0 +1,28 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+browser = webdriver.Chrome()
+browser.get("http://127.0.0.1:8000/sa/login/?next=/sa/")
+time.sleep(1)
+username = browser.find_element_by_id("id_username")
+password = browser.find_element_by_id("id_password")
+username.send_keys("palak1")
+password.send_keys("palak1")
+login_attempt = browser.find_element_by_xpath("//*[@type='submit']")
+login_attempt.submit()
+browser.get("http://127.0.0.1:8000/sa/notify/script/")
+
+browser.find_element_by_xpath("/html/body/div[1]/div[2]/div/div/div/form/div/a[1]").click()
+time.sleep(1)
+
+name = browser.find_element_by_id("id_name")
+name.send_keys("Approval Script")
+time.sleep(1)
+
+save_attempt = browser.find_element_by_xpath("/html/body/div[1]/div[2]/div/div/div/form/div/div/button[1]")
+save_attempt.click()
+
+save_attempt = browser.find_element_by_xpath("/html/body/div[1]/div[2]/div/div/div/form/div/a[1]")
+save_attempt.click()
+
---
+++ test-cases/42.py
@@ -0,0 +1,25 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+browser = webdriver.Chrome()
+browser.get("http://127.0.0.1:8000/sa/login/?next=/sa/")
+time.sleep(1)
+username = browser.find_element_by_id("id_username")
+password = browser.find_element_by_id("id_password")
+username.send_keys("palak1")
+password.send_keys("palak1")
+login_attempt = browser.find_element_by_xpath("//*[@type='submit']")
+login_attempt.submit()
+browser.get("http://127.0.0.1:8000/sa/xtheme/theme")
+
+browser.find_element_by_xpath("//*[@id='theme_form']/div/div/div/div/a").click()
+time.sleep(1)
+
+browser.find_element_by_id("id_show_welcome_text").click()
+time.sleep(1)
+
+save_attempt = browser.find_element_by_xpath("/html/body/div[1]/div[2]/div/div/div/form/div/button")
+save_attempt.click()
+
+
