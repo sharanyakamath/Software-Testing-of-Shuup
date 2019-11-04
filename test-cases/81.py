5e5b4f5db5f9121598a71f5b6315106e98623c19
sharanyakamath <sherrykamath@gmail.com>
2019-11-04 11:03:29 +0530

    firefox tescases

---
+++ test-cases/81.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+browser = webdriver.Firefox()
+browser.get("http://127.0.0.1:8000/sa/login/?next=/sa/") 
+time.sleep(1)
+username = browser.find_element_by_id("id_username")
+password = browser.find_element_by_id("id_password")
+username.send_keys("palak")
+password.send_keys("palak123")
+login_attempt = browser.find_element_by_xpath("//*[@type='submit']")
+login_attempt.submit()
+time.sleep(1)
+browser.get("http://127.0.0.1:8000/sa/tasks/new/")
